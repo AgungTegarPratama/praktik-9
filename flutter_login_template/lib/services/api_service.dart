73b978bb9b30be0e1c084f5b73e2d3ca66a06ce5
Nur Wahidah <viraktp13@gmail.com>
2026-04-30 12:16:01 +0700

    update

--- flutter_login_template/lib/services/api_service.dart
+++ flutter_login_template/lib/services/api_service.dart
@@ -1,9 +1,10 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
 
+  // 🔵 API LAMA (JANGAN DIUBAH)
   static Future<String> checkKelompok4() async {
-
     final url = Uri.parse(
       'https://api.ppb.widiarrohman.my.id/api/2026/uts/A/kelompok4/check'
     );
@@ -17,13 +18,31 @@ class ApiService {
       print("BODY: ${response.body}");
 
       if (response.statusCode == 200) {
-        return response.body; // 🔥 LANGSUNG RETURN STRING
+        return response.body;
       } else {
         throw Exception("Status code bukan 200");
       }
 
     } catch (e) {
       throw Exception("Error API: $e");
+    }
+  }
+
+  // 🟢 TAMBAHAN: API STREAM LOGS
+  static Future<Stream<String>> getLogs() async {
+    final request = http.Request(
+      'GET',
+      Uri.parse('https://api.ppb.widiarrohman.my.id/api/ppb2/stream/logs'), // 🔴 GANTI endpoint logs kamu
+    );
+
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      return response.stream
+          .transform(utf8.decoder)
+          .transform(const LineSplitter());
+    } else {
+      throw Exception("Gagal ambil logs");
     }
   }
 }
