01d5eb48c969a27d508a2636b732a8d6636e51cc
AinunNurlita <ainunnurlita2@gmail.com>
2026-05-01 14:35:43 +0700

    update ainun

--- flutter_login_template/lib/appscreen/login_screen.dart
+++ flutter_login_template/lib/appscreen/login_screen.dart
@@ -1,2 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../services/api_service.dart';
 
- 
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text("Sign In"),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 16),
+
+                  const Text(
+                    "Welcome Back",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  const Text(
+                    "Sign in with your username and password",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Color(0xFF757575)),
+                  ),
+
+                  const SizedBox(height: 30),
+
+                  const SignInForm(),
+
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
